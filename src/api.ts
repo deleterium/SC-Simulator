164a76dca0f02f9fda5a5939366a1f4517c1084c
deleterium <rui.santana@protonmail.com>
2024-02-12 09:21:15 -0300

    Added undocumented GET_ACCOUNT_BALANCE api. Refernce commit https://github.com/signum-network/signum-node/commit/c8780c6ba16f3944b62965fd976124a6f0068831

--- src/api.ts
+++ src/api.ts
@@ -1025,6 +1025,18 @@ export class API_MICROCODE {
                 }
                 return Blockchain.getAssetCirculating(asset)
             }
+        },
+        {
+            funName: 'Get_Account_Balance',
+            opCode: 0x35,
+            execute (ContractState) {
+                const account = ContractState.B[0]
+                const asset = ContractState.B[1]
+                if (asset === 0n) {
+                    return Blockchain.getBalanceFrom(account)
+                }
+                return Blockchain.getTokenQuantityFrom(account, asset)
+            }
         }
     ]
 
